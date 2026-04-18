9efba4430a3d2d34fd77bb26f81c13425d34cb55
pularuthik002-droid <pularuthik002@gmail.com>
2026-04-18 22:43:27 +0530

    code update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     print("Hello from ruthik!")
     print("Welcome to the world of GenAI!")
     print("Git push testing... ")
+    print("This is a test for git push from vscode.")
     
 
 
